a2102ca497e66688106adb6404ef629eebd91354
rubenconejo <ruben8723@gmail.com>
2019-02-05 10:11:03 +0100

    Footer responsive xD

--- resources/views/parcial/footer.blade.php
+++ resources/views/parcial/footer.blade.php
@@ -1,18 +1,10 @@
-<!--<div class="row">
-
-    <div class="col-12 mx-auto">
-    <h3>Soy un footer</h3>
-    </div>
-
-</div> -->
 
 <footer class="footer">
-<div class="fixed-bottom p-y">
+<div class="fixed-bottom">
     <div class="container-footer">
         <div class="row">
-          <div class="col-lg-12 h-100 text-center my-auto">
-            <p class="mb-4 mb-lg-0"><a href="http://www.iesvegademijas.es/">Dto. Informática IES Vega de Mijas© 2018.</p></a>
-            
+          <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 h-100 text-center my-auto">
+            <p class="mb-lg-0 mb-md-0 mb-sm-0 mb-0" ><a href="http://www.iesvegademijas.es/">Dto. Informática IES Vega de Mijas© 2018.</p></a>
           </div>
         </div>
     </div>
