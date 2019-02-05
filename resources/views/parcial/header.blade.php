af46bd871da131a05323fcfdd769b5f350bbd542
Jose Francisco Jimenez Romero <josefrapk1@gmail.com>
2019-02-05 12:48:46 +0100

    Cambios header

--- resources/views/parcial/header.blade.php
+++ resources/views/parcial/header.blade.php
@@ -1,15 +1,20 @@
-<div class="row">
-        <div class="col-4">
-            <h2>Departamento de informática </h2>
+   
+<div class="container-fluid py-2" id="header">
+    <div class="row">
+        <div class="col-10 col-md-10 mt-2 mt-sm-2 mt-md-2 mt-lg-3">
+            <a href="#" class="titulo_header"><h2>Departamento de informática </h2></a>
         </div>    
 
 
-    <div class=" col-8 d-flex justify-content-end ">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <!--<a class="navbar-brand" href="#">Navbar</a>-->
+    <div class=" col-2 col-sm-2 d-flex justify-content-end">
+            <nav class="navbar navbar-expand-lg">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <span>
+                    <i class="line mt-1"></i>
+                    <i class="line"></i>
+                    <i class="line"></i>
+                </span></button>
+             
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                     <a class="nav-item nav-link active" href="#">Inicio <span class="sr-only">(current)</span></a>
@@ -19,4 +24,5 @@
                 </div>
             </nav>
     </div>
+</div>
 </div>
