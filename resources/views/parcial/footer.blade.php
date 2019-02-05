091d9718e6d7fe333a5f22f41802ae8288bb0054
rubenconejo <ruben8723@gmail.com>
2019-02-05 09:46:09 +0100

    añadido link al Footer

--- resources/views/parcial/footer.blade.php
+++ resources/views/parcial/footer.blade.php
@@ -11,7 +11,8 @@
     <div class="container-footer">
         <div class="row">
           <div class="col-lg-12 h-100 text-center my-auto">
-            <p class="mb-4 mb-lg-0">&copy; Dto. Informática IES Vega de Mijas© 2018.</p>
+            <p class="mb-4 mb-lg-0"><a href="http://www.iesvegademijas.es/">Dto. Informática IES Vega de Mijas© 2018.</p></a>
+            
           </div>
         </div>
     </div>
