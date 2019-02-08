a93ae8026832de43dd619e009f753c4cb6a17121
rubenconejo <ruben8723@gmail.com>
2019-02-08 14:09:55 +0100

    Cambios index

--- resources/views/parcial/header.blade.php
+++ resources/views/parcial/header.blade.php
@@ -1,12 +1,12 @@
    
 <div class="container-fluid py-2" id="header">
     <div class="row">
-        <div class="col-10 col-md-10 mt-2 mt-sm-2 mt-md-2 mt-lg-3">
+        <div class="col-10 col-md-10 mt-2 mt-sm-2 mt-md-2 mt-lg-3 order-1 order-md-0">
             <a href="#" class="titulo_header"><h2>Departamento de informática </h2></a>
         </div>    
 
 
-    <div class=" col-2 col-sm-2 d-flex justify-content-end">
+    <div class=" col-2 col-sm-2 d-flex justify-content-end order-0 order-md-1">
             <nav class="navbar navbar-expand-lg">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span>
--- resources/views/static/index.blade.php
+++ resources/views/static/index.blade.php
@@ -1,7 +1,16 @@
 @extends('layout')
 
 @section('contenido')
-<ul>
+<!------------------------------------------------------------>
+
+<!-------------------------------------------------------------->
+<div class="jumbotron">
+  <div class="container">
+    <h1>Lorem ipsum</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste saepe mollitia laborum numquam? Porro exercitationem, temporibus at dignissimos minima sit nihil, accusantium totam dolorum blanditiis eius vero optio reiciendis fuga.</p>
+    <ul>
     <li>Hola</li>
     <li>Que</li>
     <li>Ase</li>
@@ -9,5 +18,9 @@
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nesciunt iste molestiae iusto neque laboriosam consequuntur impedit aperiam perferendis corrupti explicabo voluptates officiis amet dolor vero qui, rerum non quo?</p>
 <br>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est cumque doloribus amet neque nulla similique voluptates suscipit dolor impedit repellat, delectus tenetur quibusdam quidem labore fugiat ratione nemo quod nostrum!</p>
+
+  </div>
+</div>
+<!------------------------------------------------------------------>
 
 @endsection
