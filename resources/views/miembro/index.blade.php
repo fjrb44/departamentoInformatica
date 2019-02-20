86d7f8ad871cbeb5a3a38d07f214f7d4e0364c3c
josefrapk <josefrapk1@gmail.com>
2019-02-20 14:30:19 +0100

    Modificacion vista miembros

--- resources/views/miembro/index.blade.php
+++ resources/views/miembro/index.blade.php
@@ -1,21 +1,51 @@
 @extends('layout')
 
 @section('contenido')
-    <div class="row d-flex justify-content-center">
-    @foreach ($miembros as $miembro)
-    
-        <div class="col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2 mb-2 mt-2">
-        <a href="miembros/{{$miembro->id}}" class="custom-card">
-            <div class="card h-100">
-                <img class="card-img-top w-50 mx-auto d-block" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png">
-                <div class="card-body">
-                <h5 class="card-title">{{$miembro->nombre}} {{$miembro->apellidos}}</h5>
-                <p class="card-text">{{$miembro->email}}</p>
+    <div class="row">
+        @foreach ($miembros as $miembro)
+            @if($miembro->jefe_dept)
+                <div class="d-flex justify-content-center">    
+                    <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 mb-2 mt-3">
+                        <a href="/miembros/{{$miembro->id}}" class="custom-card">
+                            <div class="card h-100">
+                                <img class="card-img-top w-50 mx-auto d-block" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png">
+                                <div class="card-body">
+                                    <h5 class="card-title d-flex justify-content-center">{{$miembro->nombre}}</h5>
+                                    <h5 class="card-title d-flex justify-content-center"> {{$miembro->apellidos}}</h5>
+                                    
+                                </div>
+                            
+                            </div>
+                        </a>
+                     </div> 
                 </div>
-            </div>
-            </a>
+            @endif
+        @endforeach
         </div>
+
+
+
+        
+        <div class="row">
+        @foreach ($miembros as $miembro)
+            @if(!$miembro->jefe_dept)
+                <div class="d-flex justify-content-center">    
+                    <div class="col-8 col-sm-5 col-md-5 col-lg-4 col-xl-8 mb-2 mt-3">
+                        <a href="/miembros/{{$miembro->id}}" class="custom-card">
+                            <div class="card h-100">
+                                <img class="card-img-top w-50 mx-auto d-block" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png">
+                                <div class="card-body">
+                                    <h5 class="card-title d-flex justify-content-center">{{$miembro->nombre}}</h5>
+                                    <h5 class="card-title d-flex justify-content-center"> {{$miembro->apellidos}}</h5>
+                                    <p class="card-text "></p>
+                                </div>
+                            
+                            </div>
+                        </a>
+                    </div>   
+                </div>
+            @endif
     
-    @endforeach
+        @endforeach
     </div>
 @endsection
