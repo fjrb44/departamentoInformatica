74ec87f33e0045cd1fd8a8f5a7455374bc21508d
josefrapk <josefrapk1@gmail.com>
2019-02-22 14:32:55 +0100

    cambios vista miembros v2

--- resources/views/miembros/index.blade.php
+++ resources/views/miembros/index.blade.php
@@ -7,13 +7,18 @@
                 <div class="d-flex justify-content-center
                                     order-1
                                     order-md-0">    
-                    <div class="col-9 col-sm-8 col-md-8 col-lg-8 col-xl-9 mb-2 mt-3">
+                    <div class="col-9
+                                col-sm-8
+                                col-md-8
+                                col-lg-8
+                                col-xl-9
+                                mb-2 mt-3">
                         <a href="/miembros/{{$miembro->id}}" class="custom-card">
                             <div class="card h-100">
-                                <img class="card-img-top w-50 mx-auto d-block" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png">
+                                <img class="card-img-top w-50 mx-auto d-block mt-3" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png">
                                 <div class="card-body">
                                     <h5 class="card-title d-flex justify-content-center">{{$miembro->nombre}}</h5>
-                                    <h5 class="card-title d-flex justify-content-center"> {{$miembro->apellidos}}</h5>
+                                    <h6 class="card-title d-flex justify-content-center"> {{$miembro->apellidos}}</h6>
                                 </div>
                             </div>
                         </a>
@@ -36,14 +41,20 @@
         <div class="row d-flex justify-content-center">
         @foreach ($miembros as $miembro)
             @if(!$miembro->jefe_dept)
-                <div class="d-flex justify-content-center col-8 col-sm-6 col-md-4 col-lg-4 col-xl-2 mb-2 mt-3">    
+                <div class="d-flex justify-content-center
+                            col-8
+                            col-sm-6
+                            col-md-4
+                            col-lg-4
+                            col-xl-2
+                            mb-2 mt-3">    
                         <a href="/miembros/{{$miembro->id}}" class="custom-card">
                             <div class="h-100">
-                                <img class="card-img-top w-75 mx-auto d-block" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png">
+                                <img class="card-img-top w-50 mx-auto d-block" src="https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png">
                                 <div class="card-body">
                                     <h5 class="card-title d-flex justify-content-center">{{$miembro->nombre}}</h5>
-                                    <h5 class="card-title d-flex justify-content-center"> {{$miembro->apellidos}}</h5>
-                                    <p class="card-text "></p>
+                                    <h6 class="card-title d-flex justify-content-center"> {{$miembro->apellidos}}</h6>
+                                    
                                 </div>
                             </div>
                         </a>
